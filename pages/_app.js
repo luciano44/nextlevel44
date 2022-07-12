062947d077ee2f904e32dc06b8188ac7f7e6e14b
luciano44 <luciano.ar6@gmail.com>
2022-07-12 20:33:14 -0300

    fix naming issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.scss";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
